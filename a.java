a8fc3722670b82787746405f2221f18da2e86a29
vermamadhav <madhavverma.hll@gmail.com>
2023-04-25 01:38:55 +0530

    remove classes

--- a.java
+++ a.java
@@ -1,52 +1,29 @@
-import java.util.Arrays;
 import java.util.*;
 public class a {
-    public static boolean issafe (char c [][] , int r , int co){
-        for (int i = r -1 ; i >=0 ; i--){
-            if (c[i][co] == 'Q'){
-                return false ;
+    public static void stockspan(int stocks[] , int span[]){
+        Stack <Integer> s = new Stack<>();
+        s.push(0);
+        span[0] =1 ;
+        for (int i = 1 ;i < stocks.length ; i++){
+            while (!s.isEmpty() && stocks[i] >= stocks[s.peek()]){
+                s.pop();
             }
-        }
-        for (int i = r -1 , j = co -1; i >=0 && j >=0; i-- , j--){
-            if (c[i][j] == 'Q'){
-                return false ;
+            if (s.isEmpty()){
+                span[i] = i + 1;
             }
-        }
-        for (int i = r -1 , j = co +1; i >=0 && j < c.length ; i-- , j++){
-            if (c[i][j] == 'Q'){
-                return false ;
+            else{
+                span[i] = i - s.peek() ;
             }
+            s.push(i);
         }
-        return true ;
-    }
-
-    public static void nqueen (char c [][] , int r){
-        if (r == c.length ){
-            
-            System.out.println();
-            for (int i = 0 ; i < 5 ; i++){
-                for (int j = 0 ; j <5 ; j++){   
-                System.out.print(c [i][j] + " ");;
-                }
-                System.out.println();
-            } 
-            return ;
-        }
-        for (int i = 0 ; i < c.length ; i++){
-            if (issafe (c , r , i)){
-                c[r][i] = 'Q';
-                nqueen (c , r+1);
-                c[r][i] = 'x';
-            }
-        }
+        
     }
     public static void main (String args[]){
-        char c[][] = new char [5][5];
-        for (int i = 0 ; i < 5 ; i++){
-            for (int j = 0 ; j <5 ; j++){
-                c [i][j] = 'x';
-            }
+        int stockPrice [] = {100,80,60,70,60,85,100};
+        int span[] = new int [stockPrice.length];
+        stockspan(stockPrice , span);
+        for(int i = 0 ; i < span.length ;i++){
+            System.out.println(span[i]);
         }
-        nqueen (c , 0);
     }
 }
